0a09cff1e04257ed80bc2a7fb553c88b74b218d5
Keiiiii25 <102275393+Keiiiii25@users.noreply.github.com>
2022-09-11 15:10:05 -0600

    Week2LabPart4 creation

---
+++ Week2LabPart4/src/week2labpart4/Week2LabPart4.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week2labpart4;
+
+/**
+ *
+ * @author Keith
+ */
+public class Week2LabPart4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("**********************************************");
+        System.out.println("**++++++++++++++++++++++++++++++++++++++++++**");
+        System.out.println("**++++++++We're no strangers to love++++++++**");
+        System.out.println("**++++++You know the rules and so do I++++++**");
+        System.out.println("**+A full commitment's what I'm thinking of+**");
+        System.out.println("**+You wouldn't get this from any other guy+**");
+        System.out.println("**++++++++++++++++++++++++++++++++++++++++++**");
+        System.out.println("**++I just wanna tell you how I'm feeling+++**");
+        System.out.println("**+++++++++Gotta make you understand++++++++**");
+        System.out.println("**++++++++++++++++++++++++++++++++++++++++++**");
+        System.out.println("**+++++++++Never gonna give you up++++++++++**");
+        System.out.println("**+++++++++Never gonna let you down+++++++++**");
+        System.out.println("**++Never gonna run around and desert you+++**");
+        System.out.println("**+++++++++Never gonna make you cry+++++++++**");
+        System.out.println("**+++++++++Never gonna say goodbye++++++++++**");
+        System.out.println("**++++Never gonna tell a lie and hurt you+++**");
+        System.out.println("**++++++++++++++++++++++++++++++++++++++++++**");        
+        System.out.println("**++++We've known each other for so long++++**");
+        System.out.println("**++++++Your heart's been aching, but+++++++**");
+        System.out.println("**++++++++You're too shy to say it++++++++++**");
+        System.out.println("**+Inside we both know what's been going on+**");
+        System.out.println("**+We know the game and we're gonna play it+**");
+        System.out.println("**++++++++++++++++++++++++++++++++++++++++++**");
+        System.out.println("**++++And if you ask me how I'm feeling+++++**");
+        System.out.println("**++Don't tell me you're too blind to see+++**");       
+        System.out.println("**++++++++++++++++++++++++++++++++++++++++++**");
+        System.out.println("**+++++++++Never gonna give you up++++++++++**");
+        System.out.println("**+++++++++Never gonna let you down+++++++++**");
+        System.out.println("**++Never gonna run around and desert you+++**");
+        System.out.println("**+++++++++Never gonna make you cry+++++++++**");
+        System.out.println("**+++++++++Never gonna say goodbye++++++++++**");
+        System.out.println("**++++Never gonna tell a lie and hurt you+++**");
+        System.out.println("**++++++++++++++++++++++++++++++++++++++++++**");
+        System.out.println("**********************************************");                                        
+    }
+    
+}
